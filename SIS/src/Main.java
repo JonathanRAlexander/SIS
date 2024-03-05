15f28951163a46b3658f6e9d0ab045b840e522c9
jonathan.alexander <jonathan.alexander@MU-C1-11.mullen.pvt.k12.co.us>
2024-03-05 09:05:48 -0700

    my test file

--- SIS/src/Main.java
+++ SIS/src/Main.java
@@ -4,11 +4,8 @@ public class Main
 		public static void main(String[] args)
 		{
 			
-			boolean noah = true;
+			
 			System.out.println("Hello World!");
-			while(noah)
-				{
-					System.out.println("Cheese");
-				}
+			
 		}
 	}
